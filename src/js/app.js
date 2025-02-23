927a120ca0568ff89500afddeeeb1220a506787e
Katt <kitcat0962@gmail.com>
2025-02-23 14:08:59 -0500

    I need to commit more often added css styling began work on main app inputs

---
+++ src/js/app.js
@@ -0,0 +1,93 @@
+import { signOut } from "firebase/auth"
+import { auth, db } from "./firebase"
+import { doc, getDoc } from "firebase/firestore"
+
+auth.onAuthStateChanged(user => {
+    if (!user)
+        window.location = "/"
+})
+
+const btn_logout = document.getElementById('btn_logout')
+btn_logout.addEventListener('click', event => {
+    signOut(auth)
+})
+
+function formatRating(element, rating) {
+    const parsedRating = Math.min(5, Math.max(0, rating))
+    element.innerHTML = ""
+    for(let i=0;i<5;i++){
+        const p = document.createElement('p')
+        p.textContent = i<parsedRating?"★":"☆"
+        element.appendChild(p)
+    }
+    // return "★".repeat(parsedRating)+"☆".repeat(5-parsedRating)
+}
+
+const form_addbook = document.getElementById('form_addbook')
+const text_bookname = document.getElementById('text_bookname')
+const text_bookauthor = document.getElementById('text_bookauthor')
+const select_genres = document.getElementById('select_genres')
+const div_rating = document.getElementById('div_rating')
+const range_rating = document.getElementById('range_rating')
+const text_url = document.getElementById('text_url')
+getDoc(doc(db, 'static', 'genres'))
+    .then(snapshot => {
+        const genres = snapshot.get('genres')
+        console.log(genres)
+        for (let genre of genres) {
+            const option = document.createElement('option')
+            option.value = genre
+            option.label = genre
+            select_genres.appendChild(option)
+        }
+    })
+    .catch(err => console.log(err))
+    
+formatRating(div_rating, parseInt(range_rating.value))
+range_rating.addEventListener('input', function(){
+    formatRating(div_rating, parseInt(this.value))
+})
+form_addbook.addEventListener('submit', function (event) {
+    event.preventDefault()
+    text_bookname.setCustomValidity('')
+    text_bookauthor.setCustomValidity('')
+    select_genres.setCustomValidity('')
+    const errors = {}
+    const name = text_bookname.value.trim()
+    if (name.length === 0) {
+        errors.text_bookname = ['empty']
+    }
+    const author = text_bookauthor.value.trim()
+    if (author.length === 0) {
+        errors.text_bookauthor = ['empty']
+    }
+    const genres = select_genres.selectedOptions
+    if (genres.length === 0) {
+        errors.select_genres = ['empty']
+    }
+    const rating = parseInt(range_rating.value)
+    const url = text_url.value.trim()
+
+    if (Object.keys(errors).length !== 0) {
+        if (errors.text_bookname)
+            text_bookname.setCustomValidity('Name cannot be empty')
+        if (errors.text_bookauthor)
+            text_bookauthor.setCustomValidity('Author cannot be empty')
+        if (errors.select_genres)
+            select_genres.setCustomValidity('Must select at least one genre')
+        return
+    }
+
+
+});
+
+
+
+
+const book = {
+    name: '',
+    author: '',
+    genres: [''],
+    rating: 10,
+    url: ''
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
-import { app } from "./firebase";
+
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { app, auth } from "./firebase";
+
+auth.onAuthStateChanged(user=>{
+    if(user)
+        window.location = "/app"
+})
+
+const googleAuth = new GoogleAuthProvider();
+
+const btn_login = document.getElementById('btn_login')
+const p_message = document.getElementById('p_message')
+const p_message_class = p_message.className
+btn_login.addEventListener('click', event => {
+    signInWithPopup(auth, googleAuth)
+        .then(result => {
+            console.log(result)
+            console.log(auth.currentUser)
+            // window.location = "/app"
+        })
+        .catch(err => {
+            console.log(err)
+            p_message.className = p_message_class + " error"
+            p_message.textContent = JSON.stringify(err)
+        })
+})
