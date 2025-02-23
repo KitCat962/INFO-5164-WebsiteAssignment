6217b50b5b8a1c8f7f4141b1edb869b1e2d3a7f8
Katt <kitcat0962@gmail.com>
2025-02-23 16:11:36 -0500

    add book, firebase, book render

---
+++ src/js/api.js
@@ -0,0 +1,52 @@
+import { addDoc, collection, deleteDoc, doc, getDocs, onSnapshot, query, updateDoc, where } from "firebase/firestore";
+import { auth, db } from "./firebase";
+
+export async function addBook(name, author, genres, rating, url) {
+    if (!auth.currentUser) return null
+    try {
+        const result = await addDoc(collection(db, `users/${auth.currentUser.uid}/books`), {
+            name,
+            author,
+            genres,
+            rating,
+            url: url ? url : null
+        })
+        return result.id
+    } catch {
+        return null
+    }
+}
+export async function removeBook(id) {
+    if (!auth.currentUser) return null
+    await deleteDoc(doc(db, `users/${auth.currentUser.uid}/books`, id))
+}
+export async function getBooks() {
+    if (!auth.currentUser) return []
+    const snapshot = await getDocs(collection(db, `users/${auth.currentUser.uid}/books`))
+    return formatCollectionData(snapshot)
+}
+export async function getBooksByGenre(genre) {
+    if (!auth.currentUser) return []
+    const snapshot = await getDocs(query(
+        collection(db, `users/${auth.currentUser.uid}/books`),
+        where("genre", "array-contains", genre)
+    ))
+    return formatCollectionData(snapshot)
+}
+export async function setBookStatus(id, status) {
+    if (!auth.currentUser) return false
+    await updateDoc(doc(db, `users/${auth.currentUser.uid}/books`, id),{
+        status: status
+    })
+    return true
+}
+export function addBookChangeListener(fn) {
+    if (!auth.currentUser) throw 'No authenticated user'
+    onSnapshot(collection(db, `users/${auth.currentUser.uid}/books`), snapshot => fn(formatCollectionData(snapshot)))
+}
+function formatCollectionData(snapshot) {
+    return snapshot.docs.map(doc=>({
+        id: doc.id,
+        ...doc.data()
+    }))
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,27 @@
 import { signOut } from "firebase/auth"
 import { auth, db } from "./firebase"
-import { doc, getDoc } from "firebase/firestore"
+import { collection, doc, getDoc, onSnapshot } from "firebase/firestore"
+import { addBook, addBookChangeListener, getBooks, removeBook } from "./api"
 
 const p_user = document.getElementById('p_user')
 auth.onAuthStateChanged(user => {
     if (!user)
         window.location = "/"
-    else
+    else {
         p_user.textContent = `Logged in as ${user.displayName}`
+        addBookChangeListener(renderBooks)
+    }
 })
 
 const btn_logout = document.getElementById('btn_logout')
-btn_logout.addEventListener('click', event => {
-    signOut(auth)
-})
+btn_logout.addEventListener('click', event => signOut(auth))
 
 function formatRating(element, rating) {
     const parsedRating = Math.min(5, Math.max(0, rating))
     element.innerHTML = ""
-    for(let i=0;i<5;i++){
+    for (let i = 0; i < 5; i++) {
         const p = document.createElement('p')
-        p.textContent = i<parsedRating?"★":"☆"
+        p.textContent = i < parsedRating ? "★" : "☆"
         element.appendChild(p)
     }
     // return "★".repeat(parsedRating)+"☆".repeat(5-parsedRating)
@@ -30,6 +31,7 @@ const form_addbook = document.getElementById('form_addbook')
 const text_bookname = document.getElementById('text_bookname')
 const text_bookauthor = document.getElementById('text_bookauthor')
 const select_genres = document.getElementById('select_genres')
+console.log(select_genres.selectedOptions)
 const div_rating = document.getElementById('div_rating')
 const range_rating = document.getElementById('range_rating')
 const text_url = document.getElementById('text_url')
@@ -45,9 +47,9 @@ getDoc(doc(db, 'static', 'genres'))
         }
     })
     .catch(err => console.log(err))
-    
+
 formatRating(div_rating, parseInt(range_rating.value))
-range_rating.addEventListener('input', function(){
+range_rating.addEventListener('input', function () {
     formatRating(div_rating, parseInt(this.value))
 })
 form_addbook.addEventListener('submit', function (event) {
@@ -64,7 +66,8 @@ form_addbook.addEventListener('submit', function (event) {
     if (author.length === 0) {
         errors.text_bookauthor = ['empty']
     }
-    const genres = select_genres.selectedOptions
+    // Converts the HTMLCollection to a normal array
+    const genres = [...select_genres.selectedOptions].map(option => option.value)
     if (genres.length === 0) {
         errors.select_genres = ['empty']
     }
@@ -81,16 +84,48 @@ form_addbook.addEventListener('submit', function (event) {
         return
     }
 
-
+    addBook(name, author, genres, rating, url)
+    .then(()=>form_addbook.reset())
+    .catch()
 });
 
+const div_bookcontainer = document.getElementById('div_bookcontainer')
+function renderBooks(books) {
+    console.log(books)
+    div_bookcontainer.innerHTML = ""
+    // I wish I had JSX or something right about now
+    for (let book of books) {
+        console.log(book)
+        const bookContainer = document.createElement('div')
+        bookContainer.className = 'test'
 
+        const bookName = document.createElement('p')
+        bookName.textContent = book.name
+        bookContainer.appendChild(bookName)
 
+        const bookAuthor = document.createElement('p')
+        bookAuthor.textContent = book.author
+        bookContainer.appendChild(bookAuthor)
 
-const book = {
-    name: '',
-    author: '',
-    genres: [''],
-    rating: 10,
-    url: ''
+        console.log(book.genres)
+        const bookGenres = document.createElement('p')
+        bookGenres.textContent = book.genres.join(', ')
+        bookContainer.appendChild(bookGenres)
+
+        const bookRating = document.createElement('div')
+        bookRating.className = 'rating_display'
+        formatRating(bookRating, book.rating)
+        bookContainer.appendChild(bookRating)
+
+        const bookURL = document.createElement('a')
+        bookURL.textContent = bookURL.href = book.url
+        bookContainer.appendChild(bookURL)
+
+        const removeBookButton = document.createElement('button')
+        removeBookButton.textContent = "Delete Book"
+        removeBookButton.addEventListener('click', event => removeBook(book.id))
+        bookContainer.appendChild(removeBookButton)
+
+        div_bookcontainer.appendChild(bookContainer)
+    }
 }
